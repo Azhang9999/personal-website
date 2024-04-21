6f05def7a95651aa232e21d407a7eb4a58d01398
Andrew Zhang <and.zhang9.8@gmail.com>
2024-04-21 15:03:54 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count(3);
+            }
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {move || count()}
+        </button>
+    }
+}
+
+fn main() {
+    // Console error hook so panic stack trace can be viewed. 
+    console_error_panic_hook::set_once();
+    
+    leptos::mount_to_body(|| view! { <App/> })
+}
