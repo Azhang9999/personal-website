9b59ead62e3f8127a6d2c9b52e254b5cdf93b587
Andrew Zhang <and.zhang9.8@gmail.com>
2024-04-28 17:39:40 -0700

    added console log

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ fn App() -> impl IntoView {
 
 fn main() {
     // Console error hook so panic stack trace can be viewed. 
+    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     
     leptos::mount_to_body(|| view! { <App/> })
